d724c30efdd1319e94aedd46e0e9f0cf6a54273b
Davide D'Angelo <dangelodavide.work@gmail.com>
2025-12-08 21:05:07 +0100

    Fix build github/staging

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : "",
+};
+
+export default nextConfig;
