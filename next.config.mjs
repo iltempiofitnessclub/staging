bea3032b06a3599744b413fd67687ceefca00a2f
Davide D'Angelo <dangelodavide.work@gmail.com>
2025-12-08 21:13:42 +0100

    Fix workflow github

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,9 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig = {
   output: "export",
-
   images: {
     unoptimized: true,
   },
-
   basePath,
   assetPrefix: basePath ? `${basePath}/` : "",
 };
