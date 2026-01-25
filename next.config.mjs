c41a403a21ac002ab214b4ceba2fee33db8676c1
Davide D'Angelo <dangelodavide.work@gmail.com>
2026-01-25 20:14:20 +0100

    Img

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isGitHub = process.env.NEXT_PUBLIC_ENV === "github";
+const isGitHub = process.env.NEXT_PUBLIC_ENV === 'github';
 
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: isGitHub ? "/staging" : "",
-  assetPrefix: isGitHub ? "/staging" : "",
+  images: {
+    unoptimized: true,
+    path: isGitHub ? '/staging/_next/image' : '/_next/image',
+  },
+  basePath: isGitHub ? '/staging' : '',
+  assetPrefix: isGitHub ? '/staging' : '',
 };
 
 export default nextConfig;
